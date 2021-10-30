d878f0bcc85b71b8320a196f2ab150375de8177c
Muhammad Yaseen <yaseenranta76@gmail.com>
2021-10-30 16:53:23 +0500

    testing account

--- src/components/PermissionHelper.js
+++ src/components/PermissionHelper.js
@@ -10,8 +10,18 @@ const validate = (modulePermission) => {
   return permit.length ? true : false;
 };
 
+const categoryCheck = () => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const userPermissions = userInfo.package.category_count
+    ? userInfo.package.category_count
+    : 0;
+
+  return userPermissions === 0 ? true : false;
+};
+
 const PermissionHelper = {
   validate,
+  categoryCheck,
 };
 
 export default PermissionHelper;
