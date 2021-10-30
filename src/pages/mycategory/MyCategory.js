e49f9b3575eeef3f6a8647fdf10e47283f8480ab
=ft1@admdenim.com <ft1@admdenim.com>
2021-10-30 18:27:00 +0500

    Deploying Code
    https://instagram.com

--- src/pages/mycategory/MyCategory.js
+++ src/pages/mycategory/MyCategory.js
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from "axios";
 import Select from "react-select";
-import { Row, Col, Button, Modal } from "react-bootstrap";
-import { toast } from "react-toastify";
+import {Row, Col, Button, Modal} from "react-bootstrap";
+import {toast} from "react-toastify";
 import placeholder from "../../../src/images/placeholder.svg";
-import { createBrowserHistory } from "history";
+import {createBrowserHistory} from "history";
 export const history = createBrowserHistory({
   forceRefresh: true,
 });
@@ -15,6 +15,7 @@ class MyCategory extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showPaymentButton: false,
       myCategory: "",
       user_id: "",
       category: [],
@@ -29,15 +30,16 @@ class MyCategory extends React.Component {
       packages: "",
       package: userInfo.package.package_name,
       categoryAllow: userInfo.package.category_count,
+      package_amount: userInfo.package.package_amount,
     };
   }
 
   componentDidMount() {
     if (userInfo.access_token !== "") {
-      this.setState({ isInstagramConnected: true });
+      this.setState({isInstagramConnected: true});
     }
     // let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    this.setState({ user_id: userInfo.user_id });
+    this.setState({user_id: userInfo.user_id});
     this.fetchMyCategory();
     this.fetchSaveCategory();
     this.getPackages();
@@ -50,13 +52,13 @@ class MyCategory extends React.Component {
       .then((response) => {
         const selectPackages = [];
         const packages = response.data.message;
-        packages.map(({ package_id, package_name, package_amount }) => {
+        packages.map(({package_id, package_name, package_amount}) => {
           return selectPackages.push({
             value: package_id,
             label: package_name,
           });
         });
-        this.setState({ packages: selectPackages });
+        this.setState({packages: selectPackages});
       })
       .catch(function (error) {
         console.log(error);
@@ -69,14 +71,14 @@ class MyCategory extends React.Component {
       .then((response) => {
         const selectCategories = [];
         const myCategories = response.data.message;
-        myCategories.map(({ category_id, category_name, image_url }) => {
+        myCategories.map(({category_id, category_name, image_url}) => {
           return selectCategories.push({
             value: category_id,
             label: category_name,
             image: image_url,
           });
         });
-        this.setState({ myCategory: selectCategories });
+        this.setState({myCategory: selectCategories});
       })
       .catch((error) => {
         console.log(error);
@@ -90,7 +92,7 @@ class MyCategory extends React.Component {
         const saveCategories = [];
         //const myCategories = response.data.message;
         const optionCategories = response.data.message;
-        optionCategories.map(({ category_id, category_name, image_url }) => {
+        optionCategories.map(({category_id, category_name, image_url}) => {
           return saveCategories.push({
             value: category_id,
             label: category_name,
@@ -134,7 +136,7 @@ class MyCategory extends React.Component {
         : this.state.saveCategories.map((category) => {
             return category.value;
           });
-    this.setState({ loading: true });
+    this.setState({loading: true});
 
     await axios
       .put(`/users/revise/categories/${userInfo.user_id}`, {
@@ -144,43 +146,45 @@ class MyCategory extends React.Component {
         let categoryResponse = response.data;
         if (categoryResponse.success) {
           toast.success(categoryResponse.message);
-          this.setState({ categoryError: "" });
-          this.setState({ loading: false });
+          this.setState({categoryError: ""});
+          this.setState({loading: false});
           this.fetchSaveCategory();
         }
       })
       .catch((err) => {
         console.log(err.response, "err");
-        this.setState({ loading: false });
-        this.setState({ categoryError: err.response.data.message });
+        this.setState({loading: false});
+        this.setState({categoryError: err.response.data.message});
       });
     // }
   };
 
-  UpgradePlan = () => {
-    alert("Please Contact Customer Support Team");
+  handlePackage = () => {
+    this.setState({showPaymentButton: true});
   };
+
   toggleModal = () => {
-    const { modal } = this.state;
+    const {modal} = this.state;
     this.setState({
       modal: !modal,
     });
   };
+
   disconnect = async () => {
-    this.setState({ loadingInsta: true });
+    this.setState({loadingInsta: true});
     await axios
       .put(`/users/revise/disconnectInstagram/disconnected`, {
         user_id: this.state.user_id,
       })
       .then((response) => {
-        this.setState({ modal: false });
-        this.setState({ loadingInsta: false });
+        this.setState({modal: false});
+        this.setState({loadingInsta: false});
         localStorage.removeItem("access_token");
         localStorage.setItem("userInfo", JSON.stringify(response.data.data));
         history.push("/connect");
       })
       .catch((err) => {
-        this.setState({ loadingInsta: false });
+        this.setState({loadingInsta: false});
         console.log(err.response, "err");
       });
   };
@@ -190,55 +194,59 @@ class MyCategory extends React.Component {
     return (
       <div className="category-page">
         <div
-          className={
-            this.props.className ? this.props.className : "container-fluid"
-          }
+          className={this.props.className ? this.props.className : "container"}
         >
           <div className="justify-content-md-center">
             <div className="connections mt-5">
+              <div className="page-title text-center">
+                <h3>Account Setup</h3>
+              </div>
               <div className="white-box mt-5">
                 <h5 className="page-title line-heading">Subscription Plan</h5>
                 <Row>
                   <Col md={8}>
                     <h3 className="package_name">
-                      {userInfo1.package ? userInfo1.package.package_name : ""} Account
-                      {/* Individual */}
+                      {userInfo1.package ? userInfo1.package.package_name : ""}{" "}
+                      Account {"  "}
+                      <span>${this.state.package_amount}/Month</span>
                     </h3>
-                    {/* {userInfo1.package.category_count !== 0 ? ( */}
+
                     <div className="category_count">
-                      You have only {this.state.categoryAllow} categories
-                      allowed in this plan
+                      You have {this.state.categoryAllow} categories allowed in
+                      this plan
                     </div>
-                    {/* ) : null} */}
-                  </Col>
-                  <Col md={4} className="mt-5">
-                    {/* {userInfo1.package.category_count !== 0 ? ( */}
-
-                    <Select
-                      options={this.state.packages}
-                      placeholder="Select package"
-                      value={{
-                        label: this.state.package,
-                        value: this.state.package,
-                      }}
-
-                      //                      onChange={(options, e) => this.handleSelect(e, options)}
-                    />
-                    {/* <Button
-                      variant="primary"
-                      className="btn-block cat-right-btn"
-                      onClick={this.UpgradePlan}
-                    >
-                      Upgrade Plan
-                    </Button> */}
-                    {/* ) : null} */}
+                    <Row className="mt-4">
+                      <Col md={6}>
+                        <Select
+                          options={this.state.packages}
+                          placeholder="Select package"
+                          value={{
+                            label: this.state.package,
+                            value: this.state.package,
+                          }}
+                          onChange={(options, e) =>
+                            this.handlePackage(e, options)
+                          }
+                        />
+                      </Col>
+                      {this.state.showPaymentButton && (
+                        <Col md={6}>
+                          <Button>
+                            <i className="fa fa-payment" />
+                            &nbsp;&nbsp;Payment
+                          </Button>
+                        </Col>
+                      )}
+                    </Row>
                   </Col>
                 </Row>
               </div>
               <form onSubmit={this.handleSubmit} className="white-box">
-                <h5 className="page-title line-heading">Subscribed Categories</h5>
+                <h5 className="page-title line-heading">
+                  Subscribed Categories
+                </h5>
                 <Row>
-                  <Col md={8}>
+                  <Col md={12}>
                     <label>Select Category: </label>
                     {this.state.saveCategories === "" ? null : (
                       <Select
@@ -278,7 +286,54 @@ class MyCategory extends React.Component {
                       )}
                     </Row>
                   </Col>
-                  <Col md={4}>
+                </Row>
+
+                <Row className="white-box">
+                  <Col md={12}>
+                    <h5 className="page-title line-heading">Connections</h5>
+                  </Col>
+                  <Col md={8}>
+                    <div className="category_count">Connection Status</div>
+                  </Col>
+                  <Col md={4} className="text-right">
+                    {(userInfo1.username !== "" && !this.props.username) ||
+                    (userInfo1.username !== "" &&
+                      this.props.username !== "") ? (
+                      <>
+                        <div className="connected-text text-center mb-2">
+                          Connected Instagram: @
+                          {userInfo1.username !== ""
+                            ? userInfo1.username
+                            : this.props.username}
+                        </div>
+                        <Button
+                          variant="primary"
+                          className="btn-block cat-right-btn"
+                          onClick={() => {
+                            this.setState({modal: true});
+                          }}
+                        >
+                          Disconnect Instagram
+                        </Button>
+                      </>
+                    ) : (
+                      <>
+                        <Button
+                          onClick={() => {
+                            window.location.replace(this.props.url);
+                          }}
+                          variant="primary"
+                          className="btn-block cat-right-btn"
+                        >
+                          <i className="fa fa-instagram" />
+                          &nbsp;&nbsp; Connect Instagram
+                        </Button>
+                      </>
+                    )}
+                  </Col>
+                </Row>
+                <Row className="">
+                  <Col md={3}>
                     <Button
                       variant="primary"
                       type="submit"
@@ -294,104 +349,42 @@ class MyCategory extends React.Component {
                   </Col>
                 </Row>
               </form>
-
-              <Row className="white-box">
-                <Col md={12}>
-                  {/* {this.state.alert && this.props.errorInsta ? (
-                    <Alert
-                      variant="danger"
-                      onClose={() => {
-                        this.setState({ alert: false });
-                      }}
-                      dismissible
-                    >
-                      <div className="h5">{this.props.errorInsta}</div>
-                    </Alert>
-                  ) : null} */}
-                </Col>
-                <Col md={12}>
-                  <h5 className="page-title line-heading">Connections</h5>
-                </Col>
-                <Col md={8}>
-                  <div className="category_count">
-                   Connection Status
-                  </div>
-                </Col>
-                <Col md={4} className="text-right">
-                  {(userInfo1.username !== "" && !this.props.username) ||
-                  (userInfo1.username !== "" && this.props.username !== "") ? (
-                    <>
-                      <div className="connected-text text-center mb-2">
-                        Connected Instagram: @
-                        {userInfo1.username !== ""
-                          ? userInfo1.username
-                          : this.props.username}
-                      </div>
-                      <Button
-                        variant="primary"
-                        className="btn-block cat-right-btn"
-                        onClick={() => {
-                          this.setState({ modal: true });
-                        }}
-                      >
-                        Disconnect Instagram
-                      </Button>
-                      <Modal
-                        show={this.state.modal}
-                        onHide={this.toggleModal}
-                        className="change-password"
-                        centered
-                      >
-                        <Modal.Header closeButton>
-                          <Modal.Title>Disconnect Instagram</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body className="bg-white">
-                          Are you sure you want to disconnect
-                          <span class="strong">
-                            {" "}
-                            @{userInfo1.username}
-                          </span>{" "}
-                          account from Konnect.bio? This will remove all your
-                          content from our platform.
-                          <p>This action is not reversible.</p>
-                        </Modal.Body>
-                        <Modal.Footer>
-                          <Button
-                            onClick={() => {
-                              this.setState({ modal: false });
-                            }}
-                          >
-                            Close
-                          </Button>
-                          <Button
-                            className="disconnect-btn"
-                            onClick={this.disconnect}
-                            disabled={this.state.loadingInsta ? true : false}
-                          >
-                            Yes
-                          </Button>
-                        </Modal.Footer>
-                      </Modal>
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        onClick={() => {
-                          window.location.replace(this.props.url);
-                        }}
-                        variant="primary"
-                        className="btn-block cat-right-btn"
-                      >
-                        <i className="fa fa-instagram" />
-                        &nbsp;&nbsp; Connect Instagram
-                      </Button>
-                    </>
-                  )}
-                </Col>
-              </Row>
             </div>
           </div>
         </div>
+
+        <Modal
+          show={this.state.modal}
+          onHide={this.toggleModal}
+          className="change-password"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Disconnect Instagram</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="bg-white">
+            Are you sure you want to disconnect
+            <span class="strong"> @{userInfo1.username}</span> account from
+            Konnect.bio? This will remove all your content from our platform.
+            <p>This action is not reversible.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={() => {
+                this.setState({modal: false});
+              }}
+            >
+              Close
+            </Button>
+            <Button
+              className="disconnect-btn"
+              onClick={this.disconnect}
+              disabled={this.state.loadingInsta ? true : false}
+            >
+              Yes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
