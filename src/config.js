7490599b528c11794ff0934fb3815f5a72b53844
Muhammad Yaseen <yaseenranta76@gmail.com>
2021-11-04 17:14:46 +0500

    url http fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-// const hostApi = process.env.NODE_ENV === "development" ? "http://172.16.1.85:9000": "https://api.konnect.bio";
+// const hostApi =
+//   process.env.NODE_ENV === "development"
+//     ? "http://172.16.1.85:9000"
+//     : "https://api.konnect.bio";
 const hostApi = "https://api.konnect.bio";
 const portApi = process.env.NODE_ENV === "development" ? "" : "";
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/v1`;
