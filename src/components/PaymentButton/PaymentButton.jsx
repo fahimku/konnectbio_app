b54d1e291b8f547f136a8dd246d9fc17ab8819ca
kbfahim <kbfahim@konnect.bio>
2021-11-08 11:26:40 +0500

    live packages added

--- src/components/PaymentButton/PaymentButton.jsx
+++ src/components/PaymentButton/PaymentButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const PaymentButton = (props) => {
   const userId = props.userId;
@@ -9,33 +9,44 @@ const PaymentButton = (props) => {
     return (
       <>
         <form
-          action="https://demo.globalgatewaye4.firstdata.com/pay"
+          action="https://checkout.globalgatewaye4.firstdata.com/pay"
           id="micro_influencer_form_monthly"
           method="post"
         >
           <input type="hidden" name="x_user_id" value={userId} />
           <input type="hidden" name="x_package_id" value={packageId} />
-          <input type="hidden" name="x_login" value="WSP-ADM-D-DAwMRwCArg" />
+          <input type="hidden" name="x_recurring_billing" value="TRUE" />
+          <input name="x_recurring_billing_amount" value="10" type="hidden" />
+          <input
+            name="x_recurring_billing_id"
+            value="MB-KONNE-66-1635196"
+            type="hidden"
+          />
+          <input
+            type="hidden"
+            name="x_login"
+            value="WSP-KONNE-j&amp;AXMgDiJw"
+          />
           <input type="hidden" name="x_show_form" value="PAYMENT_FORM" />
           <input
             type="hidden"
             name="x_fp_sequence"
-            value="1636096318295562651"
+            value="16360932572458729698"
           />
           <input
             type="hidden"
             name="x_fp_hash"
-            value="PNB-1.0-2dd1b83fafe52f772268a681945cee554e82e57e"
+            value="PNB-1.0-597b90255badd0b17fa5c5dc4fc1ca610e4977ca"
           />
           <input type="hidden" name="x_amount" value="10" />
           <input type="hidden" name="x_currency_code" value="USD" />
-          <input type="hidden" name="x_test_request" value="TRUE" />
+          <input type="hidden" name="x_test_request" value="FALSE" />
           <input type="hidden" name="x_relay_response" value="" />
           <input type="hidden" name="donation_prompt" value="" />
           <input
             type="hidden"
             name="button_code"
-            value="Pay Now Influencer Demo Account"
+            value="Pay Now Micro Influencer"
           />
         </form>
         <Button
@@ -55,33 +66,40 @@ const PaymentButton = (props) => {
     return (
       <>
         <form
-          action="https://demo.globalgatewaye4.firstdata.com/pay"
+          action="https://checkout.globalgatewaye4.firstdata.com/pay"
           id="micro_influencer_form_yearly"
           method="post"
         >
           <input type="hidden" name="x_user_id" value={userId} />
           <input type="hidden" name="x_package_id" value={packageId} />
-          <input type="hidden" name="x_login" value="WSP-ADM-D-DAwMRwCArg" />
+          <input type="hidden" name="x_recurring_billing" value="TRUE" />
+          <input name="x_recurring_billing_amount" value="96" type="hidden" />
+          <input
+            name="x_recurring_billing_id"
+            value="MB-KONNE-4-1635197"
+            type="hidden"
+          />
+          <input type="hidden" name="x_login" value="WSP-KONNE-PsjK0gDiRA" />
           <input type="hidden" name="x_show_form" value="PAYMENT_FORM" />
           <input
             type="hidden"
             name="x_fp_sequence"
-            value="16362069291731848312"
+            value="16360933072217852277"
           />
           <input
             type="hidden"
             name="x_fp_hash"
-            value="PNB-1.0-db26d195390c07e3c9dba80f59b66c206a034cce"
+            value="PNB-1.0-3b2c1b6bd72fe4f5aa52097adf8279ce0de6f80f"
           />
           <input type="hidden" name="x_amount" value="96" />
           <input type="hidden" name="x_currency_code" value="USD" />
-          <input type="hidden" name="x_test_request" value="TRUE" />
+          <input type="hidden" name="x_test_request" value="FALSE" />
           <input type="hidden" name="x_relay_response" value="" />
           <input type="hidden" name="donation_prompt" value="" />
           <input
             type="hidden"
             name="button_code"
-            value="Pay Now Influencer Demo Account"
+            value="Pay Now Micro Influencer"
           />
         </form>
         <Button
@@ -98,35 +116,37 @@ const PaymentButton = (props) => {
     return (
       <>
         <form
-          action="https://demo.globalgatewaye4.firstdata.com/pay"
+          action="https://checkout.globalgatewaye4.firstdata.com/pay"
           id="influencer_monthly_form"
           method="post"
         >
-          <input type="hidden" name="x_login" value="WSP-ADM-D-DAwMRwCArg" />
+          <input type="hidden" name="x_user_id" value={userId} />
+          <input type="hidden" name="x_package_id" value={packageId} />
+          <input type="hidden" name="x_recurring_billing" value="TRUE" />
+          <input name="x_recurring_billing_amount" value="25" type="hidden" />
+          <input
+            name="x_recurring_billing_id"
+            value="MB-KONNE-66-1635196"
+            type="hidden"
+          />
+          <input type="hidden" name="x_login" value="WSP-KONNE-A4sMaADiRQ" />
           <input type="hidden" name="x_show_form" value="PAYMENT_FORM" />
           <input
             type="hidden"
             name="x_fp_sequence"
-            value="16362127803789352822"
+            value="16360934351335682202"
           />
           <input
             type="hidden"
             name="x_fp_hash"
-            value="PNB-1.0-dc887f6f316398caef21e1bcd0d22f144f727a75"
+            value="PNB-1.0-46df6738dd859aebe5580e85564fef3d49d59221"
           />
-           <input type="hidden" name="x_user_id" value={userId} />
-          <input type="hidden" name="x_package_id" value={packageId} />
           <input type="hidden" name="x_amount" value="25" />
           <input type="hidden" name="x_currency_code" value="USD" />
-          <input type="hidden" name="x_test_request" value="TRUE" />
+          <input type="hidden" name="x_test_request" value="FALSE" />
           <input type="hidden" name="x_relay_response" value="" />
           <input type="hidden" name="donation_prompt" value="" />
-          <input
-            type="hidden"
-            name="button_code"
-            value="Pay Now Influencer Demo Account"
-          />
-  
+          <input type="hidden" name="button_code" value="Pay Now Influencer" />
         </form>
         <Button
           onClick={() => {
@@ -142,34 +162,37 @@ const PaymentButton = (props) => {
     return (
       <>
         <form
-          action="https://demo.globalgatewaye4.firstdata.com/pay"
+          action="https://checkout.globalgatewaye4.firstdata.com/pay"
           id="influencer_yearly_form"
           method="post"
         >
           <input type="hidden" name="x_user_id" value={userId} />
           <input type="hidden" name="x_package_id" value={packageId} />
-          <input type="hidden" name="x_login" value="WSP-ADM-D-DAwMRwCArg" />
+          <input type="hidden" name="x_recurring_billing" value="TRUE" />
+          <input name="x_recurring_billing_amount" value="240" type="hidden" />
+          <input
+            name="x_recurring_billing_id"
+            value="MB-KONNE-4-1635197"
+            type="hidden"
+          />
+          <input type="hidden" name="x_login" value="WSP-KONNE-h50BAQDiRg" />
           <input type="hidden" name="x_show_form" value="PAYMENT_FORM" />
           <input
             type="hidden"
             name="x_fp_sequence"
-            value="16362088123952108431"
+            value="16360934771485025201"
           />
           <input
             type="hidden"
             name="x_fp_hash"
-            value="PNB-1.0-7db3a69570641487f5e2fdcb4d4a811e31273c06"
+            value="PNB-1.0-5f952e9d47f3c9cdb3960793733d9fca6e4bbfac"
           />
           <input type="hidden" name="x_amount" value="240" />
           <input type="hidden" name="x_currency_code" value="USD" />
-          <input type="hidden" name="x_test_request" value="TRUE" />
+          <input type="hidden" name="x_test_request" value="FALSE" />
           <input type="hidden" name="x_relay_response" value="" />
           <input type="hidden" name="donation_prompt" value="" />
-          <input
-            type="hidden"
-            name="button_code"
-            value="Pay Now Influencer Demo Account"
-          />
+          <input type="hidden" name="button_code" value="Pay Now Influencer" />
         </form>
         <Button
           onClick={() => {
@@ -183,4 +206,4 @@ const PaymentButton = (props) => {
     );
   else return "";
 };
-export {PaymentButton};
+export { PaymentButton };
