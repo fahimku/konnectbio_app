53475dca47da34ce5eacb7ff4f0c2329c2046f84
kbfahim <kbfahim@konnect.bio>
2021-12-20 18:19:16 +0500

    config fixes

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@
 //     ? "http://172.16.1.157:9000"
 //     : "https://api.konnect.bio";
 // const hostApi = process.env.NODE_ENV === "development" ? "https://kbserverdev.herokuapp.com" : "https://api.konnect.bio";
-const hostApi = "https://kbserverdev.herokuapp.com";
-// const hostApi = "https://api.konnect.bio";
+// const hostApi = "https://kbserverdev.herokuapp.com";
+const hostApi = "https://api.konnect.bio";
 const portApi = process.env.NODE_ENV === "development" ? "" : "";
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/v1`;
 const baseURLApiToken = `${hostApi}${portApi ? `:${portApi}` : ``}`;
@@ -19,14 +19,14 @@ const redirectURL =
   process.env.NODE_ENV === "development"
     ? "https://app.konnect.bio"
     : "https://app.konnect.bio";
-const visitorURL =
-  process.env.NODE_ENV === "development"
-    ? "https://devp.konnect.bio"
-    : "https://devp.konnect.bio";
 // const visitorURL =
 //   process.env.NODE_ENV === "development"
-//     ? "https://konnect.bio"
-//     : "https://konnect.bio";
+//     ? "https://devp.konnect.bio"
+//     : "https://devp.konnect.bio";
+const visitorURL =
+  process.env.NODE_ENV === "development"
+    ? "https://konnect.bio"
+    : "https://konnect.bio";
 
 export default {
   redirectURL,
