c0b87fbafe7bd4bbf5668a96ac6588af6cfd77ee
=ft1@admdenim.com <ft1@admdenim.com>
2022-01-24 11:15:19 +0500

    Convert Class component to functional component
    Convert Class component to functional component

--- src/pages/subcriptionsetup/SubcriptionSetup.js
+++ src/pages/subcriptionsetup/SubcriptionSetup.js
@@ -1,28 +1,13 @@
 import React from "react";
-import axios from "axios";
-import { createBrowserHistory } from "history";
-import { toast } from "react-toastify";
 
-class SubcriptionSetup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      packageIndex: "",
-    };
-  }
-
-  render() {
-    return (
-      <div className="profile-page account-setup">
-        <div className="container-fluid">
-          <div className="mt-4 row">
-            <div className="col-md-12">
-              <h4 className="page-title">Subcription Setup</h4>
-            </div>
-          </div>
-        </div>
+export default function SubcriptionSetup() {
+  return  <div className="profile-page account-setup">
+  <div className="container-fluid">
+    <div className="mt-4 row">
+      <div className="col-md-12">
+        <h4 className="page-title">Subcription Setup</h4>
       </div>
-    );
-  }
+    </div>
+  </div>
+</div>
 }
-export default SubcriptionSetup;
