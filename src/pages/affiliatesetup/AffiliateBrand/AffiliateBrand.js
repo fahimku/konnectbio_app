6a62330c0a6fbedf34dbae611babcd3fa68035e9
kbfahim <kbfahim@konnect.bio>
2022-02-22 15:56:39 +0500

    brand section chnages

--- src/pages/affiliatesetup/AffiliateBrand/AffiliateBrand.js
+++ src/pages/affiliatesetup/AffiliateBrand/AffiliateBrand.js
@@ -1,8 +1,10 @@
 import React from "react";
 import axios from "axios";
-import { Button,Row,Col } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 import { toast } from "react-toastify";
 import Loader from "../../../components/Loader/Loader";
+import { Input } from "reactstrap";
+import { ClassNames } from "@emotion/react";
 
 class AffiliateBrand extends React.Component {
   constructor(props) {
@@ -12,6 +14,9 @@ class AffiliateBrand extends React.Component {
       loading: false,
       brandError: "",
       // myBrand: "",
+      affiliateCheck: false,
+      brandEdit: false,
+      oldBrand: "",
     };
   }
 
@@ -23,7 +28,11 @@ class AffiliateBrand extends React.Component {
     await axios
       .get(`/affiliate/getUserBrandName`)
       .then((response) => {
-        this.setState({ brand_name: response.data.data.brand_name });
+        this.setState({
+          oldBrand: response.data.data.brand_name,
+          brand_name: response.data.data.brand_name,
+          is_affiliate_enabled: response.data.data.is_affiliate_enabled,
+        });
       })
       .catch(function (error) {
         console.log(error.response);
@@ -40,16 +49,21 @@ class AffiliateBrand extends React.Component {
     e.preventDefault();
     if (this.state.brand_name === "") {
       this.setState({
-        brandError: "Please enter your brand name",
+        brandError: "Enter brand name",
       });
     } else {
       this.setState({ loading: true });
       await axios
         .post(`/affiliate/createAndUpdateBrandName`, {
           brand_name: this.state.brand_name,
+          is_affiliate_enabled: this.state.affiliateCheck,
         })
         .then((response) => {
-          this.setState({ loading: false });
+          this.setState({
+            loading: false,
+            brandEdit: false,
+            checkDisabled: undefined,
+          });
           toast.success(response.data.message);
           this.getMyBrands();
         })
@@ -64,6 +78,7 @@ class AffiliateBrand extends React.Component {
   };
 
   render() {
+    console.log(this.state.is_affiliate_enabled, "checkDisabled");
     return (
       <React.Fragment>
         <div className="container-fluid">
@@ -75,21 +90,109 @@ class AffiliateBrand extends React.Component {
                   <div className="dash_content_profile">
                     <form onSubmit={this.handleSubmit}>
                       <h5>Enter Brand Name</h5>
-                      <Row>
-                        <Col md={12}>
-                          <input
-                            type="text"
-                            name="brand_name"
-                            placeholder="Enter Brand"
-                            onInput={this.handleChange}
-                            className="form-control"
-                            defaultValue={this.state.brand_name}
-                          />
-                          <span className="text-danger col-md-12 pl-0">
-                            {this.state.brandError}
-                          </span>
+                      <Row className="brandrow">
+                        <Col xs={4}>
+                          <span>Brand Name:</span>
+                        </Col>
+                        <Col xs={8}>
+                          {!this.state.brandEdit ? (
+                            <>
+                              <div className="row">
+                                <Col xs={4}>
+                                  <span className="float-left">
+                                    {this.state.brand_name ? (
+                                      this.state.brand_name
+                                    ) : (
+                                      <Loader />
+                                    )}
+                                  </span>
+                                </Col>
+                                <Col xs={8}>
+                                  {this.state.brand_name ? (
+                                    <i
+                                      onClick={() => {
+                                        this.setState({
+                                          brandEdit: true,
+                                          checkDisabled:
+                                            !this.state.checkDisabled,
+                                        });
+                                      }}
+                                      className="fa fa-pencil edit-icon"
+                                      title="Edit"
+                                    ></i>
+                                  ) : null}
+                                </Col>
+                              </div>
+                            </>
+                          ) : (
+                            <div className="row brandInput">
+                              <Col xs={4}>
+                                <span>
+                                  <input
+                                    type="text"
+                                    name="brand_name"
+                                    placeholder="Enter Brand"
+                                    onInput={this.handleChange}
+                                    className="form-control"
+                                    defaultValue={this.state.brand_name}
+                                  />
+                                </span>
+                                {this.state.brandError && (
+                                  <span className="text-danger pl-0 error-brand">
+                                    {this.state.brandError}
+                                  </span>
+                                )}
+                              </Col>
+                              <Col xs={8}>
+                                <i
+                                  onClick={() => {
+                                    this.setState({
+                                      brandEdit: false,
+                                      checkDisabled: !this.state.checkDisabled,
+                                      brand_name: this.state.oldBrand,
+                                    });
+                                  }}
+                                  className="fa fa-times-circle-o edit-icon"
+                                  title="Reset"
+                                ></i>
+                              </Col>
+                            </div>
+                          )}
                         </Col>
                       </Row>
+                      {this.state.brand_name ? (
+                        <Row className="brandrow">
+                          <Col xs={4}>
+                            <span>
+                              {this.state.affiliateCheck
+                                ? "Disabled "
+                                : "Enable "}
+                              Affiliate:
+                            </span>
+                          </Col>
+                          <Col xs={8}>
+                            <div className="form-check custom-switch custom-switch-md">
+                              <input
+                                type="checkbox"
+                                defaultChecked={this.state.is_affiliate_enabled}
+                                className="custom-control-input"
+                                id={`customSwitch`}
+                                readOnly
+                                onChange={(e) => {
+                                  this.setState({
+                                    affiliateCheck: e.target.checked,
+                                    checkDisabled: !this.state.checkDisabled,
+                                  });
+                                }}
+                              />
+                              <label
+                                className="custom-control-label"
+                                htmlFor={`customSwitch`}
+                              ></label>
+                            </div>
+                          </Col>
+                        </Row>
+                      ) : null}
 
                       <Row>
                         <Col md={5} xl={3}>
@@ -101,7 +204,13 @@ class AffiliateBrand extends React.Component {
                             <Button
                               color="default"
                               type="submit"
-                              className="btn-block"
+                              className="btn-block mt-3"
+                              disabled={
+                                // !this.state.brandEdit ||
+                                this.state.checkDisabled === undefined
+                                  ? true
+                                  : false
+                              }
                             >
                               Save
                             </Button>
