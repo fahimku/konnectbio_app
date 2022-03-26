f81e8f2bc244364dac3b81842e3cae49aff07d37
kbfahim <kbfahim@konnect.bio>
2022-03-26 16:47:42 +0500

    fix campaign issue

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -5,32 +5,32 @@ import config from "../config";
 export const getPosts = (page, id, clr) => async (dispatch) => {
   try {
     if (id) {
-      const res = await axios.get(`${config.hostApi}/v1/shop/filter`, {
+      const res = await axios.get(`${config.hostApi}/v1/affiliate/filter`, {
         params: {
           limit: 15,
           page,
-          post_type: 'image',
-          id
-        }
-      })
-      if (clr) clr()
+          post_type: "image",
+          id,
+        },
+      });
+      if (clr) clr();
       dispatch({
         type: GET_POST,
-        payload: res.data?.message?.result
-      })
+        payload: res.data?.message?.result,
+      });
     } else {
       const res = await axios.get(`${config.hostApi}/v1/affiliate/posts`, {
         params: {
           limit: 15,
           page,
-          post_type: 'image'
-        }
-      })
-      if (clr) clr()
+          post_type: "image",
+        },
+      });
+      if (clr) clr();
       dispatch({
         type: GET_POST,
-        payload: res.data?.message?.result
-      })
+        payload: res.data?.message?.result,
+      });
     }
   } catch (err) {
     console.log(err);
