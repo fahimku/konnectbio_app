3aa1ec15c0477b49b2146fbc2fa0117b4e2e0007
kbfahim <kbfahim@konnect.bio>
2022-04-13 14:28:44 +0500

    fix caountry

--- src/pages/affiliate/AffiliateCampaign/UpdateModal.js
+++ src/pages/affiliate/AffiliateCampaign/UpdateModal.js
@@ -99,8 +99,6 @@ class UpdateModal extends React.Component {
   //   this.setState({ budget: value });
   // };
 
-
-  
   changePromoCode = (e, options, name, index) => {
     this.setState({ promoCodes: options.value });
   };
@@ -466,44 +464,43 @@ class UpdateModal extends React.Component {
 
       return exit[0] ? exit[0] : { value: "", label: "Select Country" };
     };
-    // const renderCityValue = (x, i) => {
-    //   if (x.state) {
-    //     const exit = [
-    //       { value: "all", name: "all" },
-
-    //       this.state.cities2[0].data.message,
-    //     ].filter((item) => item.name === x.city);
-
-    //     return exit[0]
-    //       ? {
-    //           value: exit[0].name,
-    //           label: exit[0].name === "all" ? "All Cities" : exit[0].name,
-    //         }
-    //       : { value: "", label: "All Cities" };
-    //   } else {
-    //     return { value: "", label: "All Cities" };
-    //   }
-    // };
     const renderCityValue = (x, i) => {
       if (x.state) {
         const exit = [
           { value: "all", name: "all" },
 
-          ...this.state.cities2[i].data.message,
+          this.state.cities2[0].data.message,
         ].filter((item) => item.name === x.city);
+
         return exit[0]
           ? {
               value: exit[0].name,
-              label: exit[0].name === "all" ? "All" : exit[0].name,
+              label: exit[0].name === "all" ? "All Cities" : exit[0].name,
             }
-          : { value: "", label: "Select City" };
+          : { value: "", label: "All Cities" };
       } else {
-        return { value: "", label: "Select City" };
+        return { value: "", label: "All Cities" };
       }
     };
+    // const renderCityValue = (x, i) => {
+    //   if (x.state) {
+    //     const exit = [
+    //       { value: "all", name: "all" },
+
+    //       ...this.state.cities2[i].data.message,
+    //     ].filter((item) => item.name === x.city);
+    //     return exit[0]
+    //       ? {
+    //           value: exit[0].name,
+    //           label: exit[0].name === "all" ? "All" : exit[0].name,
+    //         }
+    //       : { value: "", label: "Select City" };
+    //   } else {
+    //     return { value: "", label: "Select City" };
+    //   }
+    // };
 
     return (
-      
       <React.Fragment>
         <Formsy.Form
           onValidSubmit={() =>
