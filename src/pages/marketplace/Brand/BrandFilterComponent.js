b72143beb8645048abcaf346f072221f5cbb42fc
kbfahim <kbfahim@konnect.bio>
2022-04-16 12:38:21 +0500

    fix key

--- src/pages/marketplace/Brand/BrandFilterComponent.js
+++ src/pages/marketplace/Brand/BrandFilterComponent.js
@@ -126,7 +126,7 @@ function BrandFilterComponent({ title }) {
           </button>
         </Modal.Header>
         <Modal.Body className="bg-white transection-detail">
-          <BrandMarketPlace brandId={brandId} type="marketplace" />
+          <BrandMarketPlace brandId={brandId} type="marketplace" catId="all" />
         </Modal.Body>
       </Modal>
     </React.Fragment>
